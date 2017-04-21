3c8cc64b2787970eff2a22f166883d0a46287e3b
Mu Xu 许牧 <moonxu0501@gmail.com>
2017-04-21 13:53:50 +0800

    Create C6_Q2_Basketball.java

---
+++ Chapter6/C6_Q2_Basketball.java
@@ -0,0 +1,7 @@
+
+public class C6_Q2_Basketball 
+{
+	//The probability of winning game 1 is: p^3
+	//The probability of winning game 2 is: 3 * p^2 - 2 * p^3
+	//When 0 < p < 0.5, choose game1; When 0.5 < p < 1, choose game2; Else, either is OK
+}
