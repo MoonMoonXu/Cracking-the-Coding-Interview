48f1803be928f30dec6b74ca4ee5742db5ad790a
Mu Xu 许牧 <moonxu0501@gmail.com>
2017-04-21 13:54:10 +0800

    Create C6_Q3_Dominos.java

---
+++ Chapter6/C6_Q3_Dominos.java
@@ -0,0 +1,7 @@
+
+public class C6_Q3_Dominos
+{
+	//Each domino should take up one white and one black square,
+	//but there are only 30 black squares while 32 white squares,
+	//So it is impossible to use 31 dominos to cover the entire board.
+}
