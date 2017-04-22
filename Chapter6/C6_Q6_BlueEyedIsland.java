ad0d0290f4782ee397c2b1e06ca2d024492b7084
Mu Xu 许牧 <moonxu0501@gmail.com>
2017-04-22 11:26:46 +0800

    Create C6_Q6_BlueEyedIsland.java

---
+++ Chapter6/C6_Q6_BlueEyedIsland.java
@@ -0,0 +1,6 @@
+
+public class C6_Q6_BlueEyedIsland
+{
+	//Generalize from 1, 2, to n
+	//The answer is: it will take n nights for n people to leave. In fact, they leave together at the nth night
+}
