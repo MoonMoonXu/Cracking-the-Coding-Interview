28c65f825b332a1e5586ca42ccc282e20154024b
Mu Xu 许牧 <moonxu0501@gmail.com>
2017-04-24 10:24:34 +0800

    Create C6_Q9_100Lockers.java

---
+++ Chapter6/C6_Q9_100Lockers.java
@@ -0,0 +1,7 @@
+
+public class C6_Q9_100Lockers 
+{
+	//Door n is toggled on rounds of each factor of n.
+	//If n is a square, n will be left open, because it has an odd number of factors
+	//The number of open doors is the number of perfect squares
+}
